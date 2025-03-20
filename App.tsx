5eeed99ad065c5aeb80bca29b8c436aa57553ce5
tanawatmix <s6552410002@sau.ac.th>
2025-03-20 10:08:32 +0700

    v.1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MoneyShare from './MoneyShare';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <MoneyShare />
     </View>
   );
 }
@@ -15,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
---
+++ MoneyShare.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MoneyShare = () => {
+  return (
+    <View>
+      <Text>MoneyShare</Text>
+    </View>
+  )
+}
+
+export default MoneyShare
